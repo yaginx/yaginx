1cd068521fbc80468a58e1cef3a42d62aedf02d6
DukeCheng <duke@feinian.net>
2024-02-15 17:03:56 +0800

    Update ReverseProxyBuilder

--- backend/Yaginx/ReverseProxyBuilder.cs
+++ backend/Yaginx/ReverseProxyBuilder.cs
@@ -4,101 +4,98 @@ using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using System.Text.RegularExpressions;
-public partial class YaginxAppConfigure
+
+public static class ReverseProxyBuilder
 {
-    public static class ReverseProxyBuilder
+    public static void ProxyBuilder(IReverseProxyApplicationBuilder app) => app.Use(ProxyForwarder);
+
+    private static async Task ProxyForwarder(HttpContext context, Func<Task> next)
     {
-        public static void ProxyBuilder(IReverseProxyApplicationBuilder app)
+        var serviceProvider = context.RequestServices;
+        var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+        var logger = loggerFactory.CreateLogger("Yaginx.ProxyForwarder");
+        var moinitorInterfacePattern = serviceProvider.GetService<IOptions<MonitorInterafceOption>>()?.Value;
+        try
         {
-            app.Use(async (context, next) =>
+            var requestPath = context.Request.Path.Value;
+            var isRecordRequestLog = moinitorInterfacePattern.MatchPattern.IsNotNullOrWhitespace() && Regex.IsMatch(requestPath, moinitorInterfacePattern.MatchPattern, RegexOptions.IgnoreCase);
+
+            if (isRecordRequestLog)
             {
-                var serviceProvider = context.RequestServices;
-                var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
-                var logger = loggerFactory.CreateLogger("Yaginx.ProxyForwarder");
-                var moinitorInterfacePattern = serviceProvider.GetService<IOptions<MonitorInterafceOption>>()?.Value;
-                try
-                {
-                    var requestPath = context.Request.Path.Value;
-                    var isRecordRequestLog = moinitorInterfacePattern.MatchPattern.IsNullOrEmpty() && Regex.IsMatch(requestPath, moinitorInterfacePattern?.MatchPattern, RegexOptions.IgnoreCase);
+                var requestLogger = loggerFactory.CreateLogger("Yaginx.ProxyForwarder.RequstLog");
+                var cancellationToken = context.RequestAborted;
+                using var requestStream = new MemoryStream();
+                using var responseStream = new MemoryStream();
+                //处理Request Stream
+                var originalRequestBody = context.Request.Body;
+                context.Request.Body = requestStream;
 
-                    if (!isRecordRequestLog)
-                    {
-                        await next();
-                    }
-                    else
-                    {
-                        var requestLogger = loggerFactory.CreateLogger("Yaginx.ProxyForwarder.RequstLog");
-                        var cancellationToken = context.RequestAborted;
-                        using (var requestStream = new MemoryStream())
-                        using (var responseStream = new MemoryStream())
-                        {
-                            //处理Request Stream
-                            var originalRequestBody = context.Request.Body;
-                            context.Request.Body = requestStream;
+                //处理Response Stream
+                var originalResponseBody = context.Response.Body;
+                context.Response.Body = responseStream;
 
-                            //处理Response Stream
-                            var originalResponseBody = context.Response.Body;
-                            context.Response.Body = responseStream;
+                //设置RequestBody的新值
+                await originalRequestBody.CopyToAsync(context.Request.Body, 4096, cancellationToken);
+                context.Request.Body.SafeSeekToBegin();
 
-                            //设置RequestBody的新值
-                            await originalRequestBody.CopyToAsync(context.Request.Body, 4096, cancellationToken);
-                            context.Request.Body.SafeSeekToBegin();
+                await RecordRequestContentAsync(context, requestLogger);
 
-                            await RecordRequestContentAsync(context, requestLogger);
+                await next();
 
-                            await next();
+                await RecordResponseContentAsync(context, requestLogger);
 
-                            await RecordResponseContentAsync(context, requestLogger);
-
-                            context.Response.Body = originalResponseBody;
-                            responseStream.SafeSeekToBegin();
-                            await responseStream.CopyToAsync(context.Response.Body, 4096, cancellationToken);
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    // 记录异常信息
-                    logger.LogError(0, ex, "An error occurred while processing the proxy request for {Url}", context.Request.GetDisplayUrl());
-
-                    // 向客户端返回错误响应
-                    context.Response.StatusCode = 200;
-                    var message = $"Yaginx Error, {ex.FullMessage()}";
-                    var acceptHeader = context.Request.Headers.Accept.ToString() ?? string.Empty;
-                    if (acceptHeader.Contains("application/json"))
-                    {
-                        context.Response.ContentType = "application/json";
-                        await context.Response.WriteAsync(JsonConvert.SerializeObject(new { flag = false, err = message }));
-                    }
-                    else if (acceptHeader.Contains("text/html"))
-                    {
-                        context.Response.ContentType = "text/html";
-                        await context.Response.WriteAsync($"<html><body><h1>{message}</h1></body></html>");
-                    }
-                    else
-                    {
-                        context.Response.ContentType = "text/plain";
-                        await context.Response.WriteAsync(message);
-                    }
-                }
-            });
+                context.Response.Body = originalResponseBody;
+                responseStream.SafeSeekToBegin();
+                await responseStream.CopyToAsync(context.Response.Body, 4096, cancellationToken);
+            }
+            else
+            {
+                await next();
+            }
         }
-        private static async Task RecordResponseContentAsync(HttpContext context, ILogger requestLogger)
+        catch (Exception ex)
         {
-            context.Response.Body.SafeSeekToBegin();
-            var responseStreamReader = new StreamReader(context.Response.Body);
-            var responseString = await responseStreamReader.ReadToEndAsync();
-            requestLogger.LogInformation($"{context.Connection.Id}:RSP:{context.Request.GetDisplayUrl()}:{responseString.GetLimitString(out var _)}");
-            context.Response.Body.SafeSeekToBegin();
-        }
+            // 记录异常信息
+            logger.LogError(0, ex, "An error occurred while processing the proxy request for {Url}", context.Request.GetDisplayUrl());
 
-        private static async Task RecordRequestContentAsync(HttpContext context, ILogger requestLogger)
-        {
-            var requestStreamReader = new StreamReader(context.Request.Body);
-            var requestString = await requestStreamReader.ReadToEndAsync();
-            requestLogger.LogInformation($"{context.Connection.Id}:REQ:{context.Request.GetDisplayUrl()}:{requestString.GetLimitString(out var _)}");
-            context.Request.Body.SafeSeekToBegin();
+            // 向客户端返回错误响应
+            context.Response.StatusCode = 200;
+            var message = $"Yaginx Error, {ex.FullMessage()}";
+            var acceptHeader = context.Request.Headers.Accept.ToString() ?? string.Empty;
+            if (acceptHeader.Contains("application/json"))
+            {
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync(JsonConvert.SerializeObject(new { flag = false, err = message }));
+            }
+            else if (acceptHeader.Contains("text/html"))
+            {
+                context.Response.ContentType = "text/html";
+                await context.Response.WriteAsync($"<html><body><h1>{message}</h1></body></html>");
+            }
+            else
+            {
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync(message);
+            }
         }
     }
+
+    private static async Task RecordResponseContentAsync(HttpContext context, ILogger requestLogger)
+    {
+        context.Response.Body.SafeSeekToBegin();
+        var responseStreamReader = new StreamReader(context.Response.Body);
+        var responseString = await responseStreamReader.ReadToEndAsync();
+        requestLogger.LogInformation($"{context.Connection.Id}:RSP:{context.Request.GetDisplayUrl()}:{responseString.GetLimitString(out var _)}");
+        context.Response.Body.SafeSeekToBegin();
+    }
+
+    private static async Task RecordRequestContentAsync(HttpContext context, ILogger requestLogger)
+    {
+        var requestStreamReader = new StreamReader(context.Request.Body);
+        var requestString = await requestStreamReader.ReadToEndAsync();
+        requestLogger.LogInformation($"{context.Connection.Id}:REQ:{context.Request.GetDisplayUrl()}:{requestString.GetLimitString(out var _)}");
+        context.Request.Body.SafeSeekToBegin();
+    }
 }
+
 
