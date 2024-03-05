e21ba712dd57c968d4aee101c864152a635652a5
DukeCheng <duke@feinian.net>
2024-03-05 11:52:09 +0800

    Update Loader

--- backend/LettuceEncrypt/YaginxAcmeLoaders/YaginxAcmeCertificateLoader.cs
+++ backend/LettuceEncrypt/YaginxAcmeLoaders/YaginxAcmeCertificateLoader.cs
@@ -53,46 +53,58 @@ namespace Yaginx.YaginxAcmeLoaders
             {
                 var waitSconds = RandomNumberGenerator.GetInt32(30, 120);// 每隔30~120秒检查一次
                 await Task.Delay(TimeSpan.FromSeconds(waitSconds), stoppingToken);
-                var domains = await _certificateDomainRepsitory.GetFreeCertDomainAsync();
-                foreach (var domain in domains)
+
+                try
                 {
-                    using var acmeStateMachineScope = _serviceScopeFactory.CreateScope();
-
-                    try
+                    var domains = await _certificateDomainRepsitory.GetFreeCertDomainAsync();
+                    foreach (var domain in domains)
                     {
-                        IAcmeState state = acmeStateMachineScope.ServiceProvider.GetRequiredService<YaginxServerStartupState>();
-                        if (state is AcmeState acmeState)
-                        {
-                            acmeState.Context.CurrentDomain = domain;
-                            acmeState.Context.EmailAddress = "duke@feinian.net";
-                        }
-
-                        while (state.ContinueState == AcmeStateContinueStatus.Continue && !stoppingToken.IsCancellationRequested)
-                        {
-                            //if (state.ContinueState == AcmeStateContinueStatus.Continue)
-                            //{
-                            //    _logger.LogInformation($"Apply cert SUCCESS for domain {domain}!");
-                            //    break;
-                            //}
-                            _logger.LogTrace("ACME state transition: moving to {stateName}", state.GetType().Name);
-                            state = await state.MoveNextAsync(stoppingToken);
-                        }
-                    }
-                    catch (OperationCanceledException)
-                    {
-                        _logger.LogDebug("State machine cancellation requested. Exiting...");
-                    }
-                    catch (AggregateException ex) when (ex.InnerException != null)
-                    {
-                        await _certificateDomainRepsitory.UnFreeDomainAsync(domain, ex.InnerException.Message);
-                        _logger.LogError(0, ex.InnerException, "ACME state machine encountered unhandled error");
-                    }
-                    catch (Exception ex)
-                    {
-                        await _certificateDomainRepsitory.UnFreeDomainAsync(domain, ex.Message);
-                        _logger.LogError(0, ex, "ACME state machine encountered unhandled error");
+                        await ProcessDomain(domain, stoppingToken);
                     }
                 }
+                catch (Exception ex)
+                {
+                    _logger.LogError(0, ex, "Certificate Loader Error");
+                }
+            }
+        }
+
+        private async Task ProcessDomain(string domain, CancellationToken stoppingToken)
+        {
+            var acmeStateMachineScope = _serviceScopeFactory.CreateScope();
+            try
+            {
+                IAcmeState state = acmeStateMachineScope.ServiceProvider.GetRequiredService<YaginxServerStartupState>();
+                if (state is AcmeState acmeState)
+                {
+                    acmeState.Context.CurrentDomain = domain;
+                    acmeState.Context.EmailAddress = "duke@feinian.net";
+                }
+
+                while (state.ContinueState == AcmeStateContinueStatus.Continue && !stoppingToken.IsCancellationRequested)
+                {
+                    //if (state.ContinueState == AcmeStateContinueStatus.Continue)
+                    //{
+                    //    _logger.LogInformation($"Apply cert SUCCESS for domain {domain}!");
+                    //    break;
+                    //}
+                    _logger.LogTrace("ACME state transition: moving to {stateName}", state.GetType().Name);
+                    state = await state.MoveNextAsync(stoppingToken);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                _logger.LogDebug("State machine cancellation requested. Exiting...");
+            }
+            catch (AggregateException ex) when (ex.InnerException != null)
+            {
+                await _certificateDomainRepsitory.UnFreeDomainAsync(domain, ex.InnerException.Message);
+                _logger.LogError(0, ex.InnerException, "ACME state machine encountered unhandled error");
+            }
+            catch (Exception ex)
+            {
+                await _certificateDomainRepsitory.UnFreeDomainAsync(domain, ex.Message);
+                _logger.LogError(0, ex, "ACME state machine encountered unhandled error");
             }
         }
     }
